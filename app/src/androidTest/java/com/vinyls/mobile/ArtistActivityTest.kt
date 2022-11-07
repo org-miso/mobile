47e1c3561d1b7a7930a531cc1dd4bb4a70296200
Carlos Javier Pita Amaya <c.pita@uniandes.edu.co>
2022-11-06 23:54:15 -0500

    Se agregan pruebas unitarias

---
+++ app/src/androidTest/java/com/vinyls/mobile/ArtistActivityTest.kt
@@ -0,0 +1,33 @@
+package com.vinyls.mobile
+
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
+import androidx.test.rule.ActivityTestRule
+import com.vinyls.mobile.view.MainActivity
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@LargeTest
+@RunWith(AndroidJUnit4::class)
+class ArtistActivityTest {
+
+    @get:Rule
+    val activityRule = ActivityTestRule(MainActivity::class.java)
+
+    @Test
+    fun testTitleDisplayArtist(){
+        Espresso.onView(ViewMatchers.withText("Álbumes"))
+            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+    }
+
+    @Test
+    fun testViewDetailArtist(){
+        println(R.id.recyclerViewAlbums)
+        Espresso.onView(ViewMatchers.withId(R.id.recyclerViewAlbums)).
+        check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+    }
+}
