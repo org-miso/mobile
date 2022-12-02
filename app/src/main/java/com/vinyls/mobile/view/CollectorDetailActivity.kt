3caf74a09080c85a0dd6a88198b9f15405cfb4d7
Franz Joseph <franzjrcarvajal@gmail.com>
2022-12-02 06:16:50 -0500

    HU06 Detalle Coleccionista

---
+++ app/src/main/java/com/vinyls/mobile/view/CollectorDetailActivity.kt
@@ -0,0 +1,30 @@
+package com.vinyls.mobile.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.vinyls.mobile.databinding.ActivityAlbumDetailBinding
+import com.vinyls.mobile.databinding.ActivityCollectorDetailBinding
+
+class CollectorDetailActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityCollectorDetailBinding
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityCollectorDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val b = intent.extras
+
+
+        binding.name.text = b!!.getString("name").toString();
+        binding.phone.text = b!!.getString("telephone").toString();
+        binding.email.text = b!!.getString("email").toString();
+
+
+
+
+    }
+}
